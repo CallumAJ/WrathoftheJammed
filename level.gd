977f2913da9cb0debb9b1c89143571771b69f44c
Penderrin <baileythomasholmes@gmail.com>
2025-01-26 19:48:26 -0800

    added nodes for enemies and temporary sprites, plus some comments and fixing layers

--- level.gd
+++ level.gd
@@ -3,7 +3,7 @@ extends Node2D
 var bulletScene: PackedScene = load("res://scenes/bullet.tscn")
 
 func _on_player_bullet(pos) -> void:
-	var bullet = bulletScene.instantiate()
+	var bullet = bulletScene.instantiate() #supposed to make a bullet (doesnt)
 	$bullets.add_child(bullet)
 	bullet.position = pos
 	
