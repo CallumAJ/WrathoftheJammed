1a2570117e3ce242aed07f5b99c51c11c37b083f
Aavarce2 <evanlam33@gmail.com>
2025-01-29 21:17:32 -0800

    aaaaa
    plats and spikes and stuff

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,36 +1,7 @@
-extends Area2D
+xtends Area2D
 
-@export var speed = 100 #bullet speed
-var fireAngle #needs to be global
-
-func _ready() -> void:
-	fireAngle = 7 #fire angle will be set with a signal, this is just a test
+@export var speed = 600
 
 func _process(delta: float) -> void:
-	if (fireAngle == 0): #these check the frame johnny is in and fires a bullet in that direction
-		position.y += speed * delta
-	
-	elif (fireAngle == 1): 
-		position.y += speed * delta
-		position.x -= speed * delta
-	
-	elif (fireAngle == 2): 
-		position.x -= speed * delta
-	
-	elif (fireAngle == 3):
-		position.y -= speed * delta
-		position.x -= speed * delta
-	
-	elif (fireAngle == 4):
-		position.y -= speed * delta
-	
-	elif (fireAngle == 5):
-		position.y -= speed * delta
-		position.x += speed * delta
-	
-	elif (fireAngle == 6):
-		position.x += speed * delta
-	
-	elif (fireAngle == 7): 
-		position.y += speed * delta
-		position.x += speed * delta
+	position.y -= speed * delta
+	print("pew")
--- scenes/level.gd
+++ scenes/level.gd
@@ -1,9 +1,4 @@
 extends Node2D
 
-var bulletScene: PackedScene = load("res://scenes/bullet.tscn")
-
-func _on_player_bullet(pos) -> void:
-	var bullet = bulletScene.instantiate() #makes a bullet as a child of the bullets node
-	$Bullets.add_child(bullet)
-	bullet.position = pos
-	
+func _on_player_player_death() -> void:
+	get_tree().reload_current_scene()
