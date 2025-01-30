ad400ba92058f3629c080167bc1ed1160b83924a
Penderrin <baileythomasholmes@gmail.com>
2025-01-29 21:31:22 -0800

    think this works

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,7 +1,36 @@
 xtends Area2D
 
-@export var speed = 600
+@export var speed = 100 #bullet speed
+var fireAngle #needs to be global
+
+func _ready() -> void:
+	fireAngle = 7 #fire angle will be set with a signal, this is just a test
 
 func _process(delta: float) -> void:
-	position.y -= speed * delta
-	print("pew")
+	if (fireAngle == 0): #these check the frame johnny is in and fires a bullet in that direction
+		position.y += speed * delta
+	
+	elif (fireAngle == 1): 
+		position.y += speed * delta
+		position.x -= speed * delta
+	
+	elif (fireAngle == 2): 
+		position.x -= speed * delta
+	
+	elif (fireAngle == 3):
+		position.y -= speed * delta
+		position.x -= speed * delta
+	
+	elif (fireAngle == 4):
+		position.y -= speed * delta
+	
+	elif (fireAngle == 5):
+		position.y -= speed * delta
+		position.x += speed * delta
+	
+	elif (fireAngle == 6):
+		position.x += speed * delta
+	
+	elif (fireAngle == 7): 
+		position.y += speed * delta
+		position.x += speed * delta
--- scenes/level.gd
+++ scenes/level.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+var bulletScene: PackedScene = load("res://scenes/bullet.tscn")
+
 func _on_player_player_death() -> void:
 	get_tree().reload_current_scene()
+
+func _on_player_bullet(pos) -> void:
+	var bullet = bulletScene.instantiate() #supposed to make a bullet (doesnt)
+	$Bullets.add_child(bullet)
+	bullet.position = pos
